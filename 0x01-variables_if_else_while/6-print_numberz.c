494da21b79dd6ed3fa2a2783c9dd021fe878aa2f
aya lotfi <ayalotfi59@gmail.com>
2023-03-17 17:46:01 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - this program prints all digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+		for (n = '0'; n <= '9'; n++)
+			putchar(n);
+	putchar('\n');
+	return (0);
+}
