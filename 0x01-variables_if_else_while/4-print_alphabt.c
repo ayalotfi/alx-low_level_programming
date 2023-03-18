387b6f8e64eda7961849e25507554f6cd6de4e99
aya lotfi <ayalotfi59@gmail.com>
2023-03-17 17:42:06 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - I want to use putchar
+ * description: writing alphabets but e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'e' && c != 'q')
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
