d758ba1b8d040e6592bc82373af6aa28b10d5e8f
aya lotfi <ayalotfi59@gmail.com>
2023-03-21 14:39:13 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,30 @@
-include "main.h"
+#include "main.h"
 /**
- * print_sign - it prints the sign of a number
- * depends on the value of n 
+ *  print_sign - it prints the sign of a number
+ * depends on the value of n
  *
- * @n: character in ascii code 
+ *  @n: character in ascii code
  *
  * Return:return 1 if n > 0 & 0 if n = 0 & -1 if
- * < 0 
+ *< 0
  */
 
-int print_sign(int n )
+int print_sign(int n)
 {
-if (n > 0) 
+if (n > 0)
 {
 _putchar(43);
-return(1);
+return (1);
 }
-else (n < 0)
+else if (n < 0)
 {
--putchar(45);
-return(-1);
+_putchar(45);
+return (-1);
 }
 else
 {
 _putchar(48);
-return(0);
+return (0);
 }
 _putchar('\n');
 }
