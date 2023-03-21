25013ac04d995846a397614e5b07acfec13199a8
aya lotfi <ayalotfi59@gmail.com>
2023-03-21 15:38:10 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,11 @@
 /**
  * add - Adds two integers and returns
  * the result
- * @a: number one 
+ * @a: number one
  * @b: number two
  * Return: Add of number one and number two
  */
 int add(int a, int b)
 {
-	return (a+b);
+	return (a + b);
 }
